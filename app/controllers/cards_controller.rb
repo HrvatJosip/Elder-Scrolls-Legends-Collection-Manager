31fe47929dcb73c90b2d4f700ecd884296929644
Josip Dorvak <jdorvak@gmail.com>
2017-03-24 21:18:23 -0400

    Add card controller

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,47 @@
+class CardsController < ApplicationController
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to @card
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:name, :text, :magicka_cost, :attack, :health, :quality, :race, :type, attributes:[])
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :cards, except: [:destroy]
   root 'static#home'
 end
